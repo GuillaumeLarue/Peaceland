157fb69481c2b8388ff4314443c46f8cfc9357ca
Trabet Nicolas <nicolas.trabet@epita.fr>
2022-05-23 21:21:52 +0200

    [SBT] Add lib

--- main/src/main/scala/Main.scala
+++ main/src/main/scala/Main.scala
@@ -1,5 +1,12 @@
+import java.util.Properties
+import org.apache.kafka.clients.producer.KafkaProducer
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
 }
+
+val props: Properties = new Properties()
+
+val producer: KafkaProducer[String,String] = new KafkaProducer
---
+++ main/src/main/scala/model/Message.scala
@@ -0,0 +1,11 @@
+package model
+
+class Message {
+  private var _timestamp = 0
+  private var _peacewatcher_long = 0
+  private val _peacewatcher_lat = 0
+  private val _peacewatcher_id = 0
+  private val _citoyen_id = 0
+  private val _citoyen_peacescore = 0
+  private val _words_arounds = 0
+}
