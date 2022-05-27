6a272134011eed85053906b780ac6ca475ce90ad
guillaume.larue <guillaume.larue@epita.fr>
2022-05-28 01:18:37 +0200

    Make main compile

--- main/src/main/scala/Main.scala
+++ main/src/main/scala/Main.scala
@@ -1,12 +1,15 @@
 import java.util.Properties
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+    val props: Properties = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer: KafkaProducer[String,String] = new KafkaProducer[String, String](props)
+    println("Hello world!")
   }
 }
-
-val props: Properties = new Properties()
-
-val producer: KafkaProducer[String,String] = new KafkaProducer
