de402738d70165a49361ba9c616407aa0f7c67bb
Enguerrand de Gentile Duquesne <enguerrand.de-gentile-duquesne@epita.fr>
2022-06-07 23:52:17 +0200

    Reformat Message.scala

--- main/src/main/scala/model/Message.scala
+++ main/src/main/scala/model/Message.scala
@@ -2,13 +2,27 @@ package model
 
 import java.time.LocalDateTime
 
-class Message(val timestampArg: LocalDateTime = LocalDateTime.now(),
-              val peacewatcherIdArg: Long = scala.util.Random.nextInt(500000),
-              val peacewatcherLongArg: Double = (-180 * 10000 + scala.util.Random.nextInt(360 * 10000).toFloat) / 10000,
-              val peacewatcherLatArg: Double = (-90 * 10000 + scala.util.Random.nextInt(180 * 10000).toFloat) / 10000,
-              val citoyenIdArg: Long = scala.util.Random.nextInt(8000000),
-              val citoyenPeacescoreArg: Int = scala.util.Random.nextInt(100)) {
+class Message(val timestamp: LocalDateTime,
+              val peacewatcherId: Long = scala.util.Random.nextInt(500000),
+              val peacewatcherLong: Double,
+              val peacewatcherLat: Double,
+              val citizenId: Long,
+              val citizenPeacescore: Int) {
+
+  def this() = this(LocalDateTime.now(),
+    scala.util.Random.nextInt(500000),
+    (-180 * 10000 + scala.util.Random.nextInt(360 * 10000).toFloat) / 10000,
+    (-90 * 10000 + scala.util.Random.nextInt(180 * 10000).toFloat) / 10000,
+    scala.util.Random.nextInt(8000000),
+    scala.util.Random.nextInt(100)
+  )
+
   override def toString: String = {
-    "timestampArg:"+ timestampArg + "; peacewatcherID:"+ peacewatcherIdArg + "; peacewatcherLongArg:"+ peacewatcherLongArg + "; peacewatcherLatArg:" + peacewatcherLatArg + "; citoyenIdArg:"+ citoyenIdArg + "; citoyenPeacescoreArg:" + citoyenPeacescoreArg
+    "timestamp:" + timestamp +
+      "; peacewatcherID:" + peacewatcherId +
+      "; peacewatcherLong:" + peacewatcherLong +
+      "; peacewatcherLat:" + peacewatcherLat +
+      "; citizenId:" + citizenId +
+      "; citizenPeacescore:" + citizenPeacescore
   }
 }
