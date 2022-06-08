cd030f8b365402732cc1bce3a93c98c37e1628e6
guillaume.larue <guillaume.larue@epita.fr>
2022-06-08 22:15:06 +0200

    [DEV] Write Json from spark stream

--- ConsumerHdfs/src/main/scala/Consumer.scala
+++ ConsumerHdfs/src/main/scala/Consumer.scala
@@ -1,13 +1,6 @@
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecords, KafkaConsumer}
-import org.apache.kafka.common.serialization.{IntegerDeserializer, StringDeserializer}
-
-import java.time.Duration
-import java.util
-import java.util.Properties
-import scala.annotation.tailrec
-import scala.jdk.CollectionConverters.IterableHasAsScala
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.Trigger
+
 
 object Consumer extends App {
 
@@ -17,32 +10,22 @@ object Consumer extends App {
     .config("spark.driver.bindAddress", "127.0.0.5")
     .getOrCreate()
 
-
   val df = spark
     .readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", "localhost:9092")
     .option("subscribe", "firsttopic")
-    .option("startingOffsets","earliest")
+    .option("startingOffsets", "earliest")
     .load()
 
   df.writeStream
     .trigger(Trigger.ProcessingTime("15 seconds"))
-    .outputMode("update")
-    .format("console")
+    .format("json")
+    // local mais fonctionne exactement pareil ex: "hdfs//namenode:namenode-port/tmp/datalake/RidesRaw"
+    .option("path", "file:/tmp/hdfs")
+    // ex: "hdfs//namenode:namenode-port/tmp/checkpoints/RidesRaw"
+    .option("checkpointLocation", "file:/tmp/checkpoint")
+    .outputMode("append")
     .start()
     .awaitTermination()
-
-  /* Useful to push in hdfs
-  df.writeStream
-      .trigger(Trigger.ProcessingTime("15 seconds"))
-      .format("parquet") //voir si ya plus adapté
-      .option("path", "file:/home/trabet/peaceland/hdfs") //local mais fonctionne exactement pareille ex: "hdfs//namenode:namenode-port/tmp/datalake/RidesRaw"
-      .option("checkpointLocation", "file:/home/trabet/peaceland/checkpoint") //ex: "hdfs//namenode:namenode-port/tmp/checkpoints/RidesRaw"
-      .outputMode("append")
-      .start()*/
-
-
-
-
 }
