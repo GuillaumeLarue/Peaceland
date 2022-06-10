2f97a03a814fadd2cb79cc7dc5bd6a66cb4014c0
guillaume.larue <guillaume.larue@epita.fr>
2022-06-10 22:44:30 +0200

    [REFACTO] remove file

---
+++ web-dev/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="addAlert">add alert</button>
+    <HelloWorld v-for="alert in alerts " :alert="alert" v-bind:key="alert"/>
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      alerts: [require('./assets/alert.json')]
+    }
+  },
+  methods: {
+    addAlert() {
+      this.alerts.push(require('./assets/alert.json'))
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  background-color: rgba(5, 5, 5, 0.5)
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
